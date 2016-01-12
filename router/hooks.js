6d5c2e4ddbb6dba2e21f311ec0313a0a317a2e70
drkchiloll <samwomack_@me.com>
2016-01-12 11:28:50 -0600

    added routes to handle Spark Webhooks

---
+++ router/hooks.js
@@ -0,0 +1,7 @@
+var hooksCntrler = require('./lib/hooksCntrler');
+
+module.exports = (app, express) => {
+  var router = express.Router();
+  router.post('/hooker', hooksCntrler.procHooks);
+  app.use(router);
+};
