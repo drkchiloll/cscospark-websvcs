6934838bc876d74f1f339d4635397f195a041b39
drkchiloll <samwomack_@me.com>
2016-01-18 18:16:27 -0500

    add morgan logging middleware for the server

--- router/auth.js
+++ router/auth.js
@@ -1,7 +1,12 @@
+var morgan = require('morgan'),
+    fs = require('fs');
+
 module.exports = (app, express, authCntrler) => {
   var router = express.Router();
 
-  router.post('/authenticate', authCntrler.authenticate);
+  var axxLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+
+  router.post('/authenticate',morgan('combined', {stream: axxLogStream}), authCntrler.authenticate);
 
   app.use(router);
 };
