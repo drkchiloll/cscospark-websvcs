58bfbedb410b358e9a6c73241825b33df15b4014
drkchiloll <samwomack_@me.com>
2016-01-22 16:54:13 -0500

    modified logging to include user (spark email addr) when accessed

--- router/auth.js
+++ router/auth.js
@@ -5,8 +5,13 @@ module.exports = (app, express, authCntrler) => {
   var router = express.Router();
 
   var axxLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+
+  morgan.token('user', (req) => {
+    if(req.body.user) return req.body.user;
+    else return;
+  });
   
-  router.use(morgan('combined', {stream: axxLogStream}));
+  router.use(morgan('[:date[clf]] :method :url :status :user-agent :user', {stream: axxLogStream}));
   router.post('/authenticate', authCntrler.authenticate);
   router.post('/access', authCntrler.access);
   router.post('/refresh', authCntrler.access);
